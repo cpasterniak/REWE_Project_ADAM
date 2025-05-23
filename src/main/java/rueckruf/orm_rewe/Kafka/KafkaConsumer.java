f3d7977ae93f61065ff02d265c1d34ee54ab5d72
Christoph Pasterniak <cpasterniak@student.tgm.ac.at>
2025-05-23 09:41:01 +0200

    Kafka complete

--- src/main/java/rueckruf/orm_rewe/Kafka/KafkaConsumer.java
+++ src/main/java/rueckruf/orm_rewe/Kafka/KafkaConsumer.java
@@ -1,5 +1,7 @@
 package rueckruf.orm_rewe.Kafka;
 
+import lombok.RequiredArgsConstructor;
+import rueckruf.orm_rewe.Service.RueckrufServiceSQL;
 import rueckruf.orm_rewe.entity.Rueckruf;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
@@ -7,15 +9,19 @@ import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Service;
 
 @Service
+@RequiredArgsConstructor
 public class KafkaConsumer {
 
+    private final RueckrufServiceSQL serviceSQL;
     private final ObjectMapper objectMapper = new ObjectMapper();
+
 
     @KafkaListener(topics = "person-topic", groupId = "group_id")
     public void consume(String message) {
         try {
             Rueckruf rueckruf = objectMapper.readValue(message, Rueckruf.class);
             System.out.println("Empfangene Person: " + rueckruf);
+            serviceSQL.createRueckruf(rueckruf);
         } catch (Exception e) {
             e.printStackTrace();
         }
